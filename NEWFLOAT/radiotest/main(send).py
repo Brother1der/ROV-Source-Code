b87849ca3f3b55045dbc4b2a819d3f4eab7d8198
Graham <grahamsw10@gmail.com>
2026-08-21 22:03:23 -0500

    updatessssssss

--- NEWFLOAT/radiotest/main(send).py
+++ NEWFLOAT/radiotest/main(send).py
@@ -1,5 +1,40 @@
+#sending end!!!
+
+from machine import Pin, I2C
+import time
+import ssd1306
 from sx1262 import SX1262
 import time
+
+vext = Pin(36, Pin.OUT)
+vext.value(0)
+
+time.sleep_ms(100)
+
+rst = Pin(21, Pin.OUT)
+rst.value(0)
+time.sleep_ms(50)
+rst.value(1)
+time.sleep_ms(100)
+
+i2c = I2C(
+    0,
+    scl=Pin(18),
+    sda=Pin(17),
+    freq=100000
+)
+
+oled = ssd1306.SSD1306_I2C(
+    64,
+    32,
+    i2c,
+    addr=0x3C
+)
+
+oled.fill(0)
+oled.text("LORA RX", 0, 0)
+oled.text("923 MHz", 0, 12)
+oled.show()
 
 sx = SX1262(
     spi_bus=1,
@@ -31,10 +66,15 @@ sx.begin(
     blocking=True
 )
 
-    msg = b"Hello World!"
+msg = b"Hello World!"
 
-    print("Sending:", msg)
+print("Sending:", msg)
+oled.fill(0)
+oled.text("LORA RX", 0, 0)
+oled.text("Sending", msg, 0, 12)
+oled.show()
 
-    err = sx.send(msg)
+err = sx.send(msg)
 
-    print("Send status:", SX1262.STATUS[err])
+print("Send status:", SX1262.STATUS[err])
+    
